0a09960a511134ddbe825f4f1648ed3c9e5ecef5
Dmitriy Demyanov <dimaprostokosmos@gmail.com>
2022-11-01 18:06:35 +0200

    Error Router

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <div id="app">
     <PosterBg :poster="posterBg"/>
     <MoviesList :list="moviesList" @changePoster="onChangePoster"/>
+    <MoviesPagination
+    :current-page="currentPage"
+     :per-page="moviesPerPage"
+     :total="moviesLength"
+     @pageChanged="onPageChanged"
+     />
   </div>
 </template>
 
@@ -12,26 +18,29 @@
 import { mapActions, mapGetters } from 'vuex';
 import PosterBg from './components/PosterBg.vue';
 import MoviesList from '@/components/MoviesList.vue';
-
+import MoviesPagination from './components/MoviesPagination'
 
 export default {
   name: "App",
   components: {
     MoviesList,
     PosterBg,
+    MoviesPagination,
   },
   data: () => ({
     posterBg:'',
   }),
   computed: {
-    ...mapGetters('movies', ['moviesList'])
+    ...mapGetters('movies', ['moviesList', 'currentPage', 'moviesPerPage', 'moviesLength' ])
   },
   methods: {
-    ...mapActions('movies', ['fetchMovies']),
+    ...mapActions('movies', ['changeCurrentPage']),
     onChangePoster(poster) {
-      console.log(poster);
       this.posterBg = poster;
     },
+    onPageChanged(page) {
+      this.changeCurrentPage(page);
+    }
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
+import Router from "vue-router"
 import App from "./App.vue";
 import store from "./store";
 import "./plugins/bootstrap";
 
 Vue.config.productionTip = false;
-
+vue.use(Router);
 new Vue({
   store,
   render: (h) => h(App),
